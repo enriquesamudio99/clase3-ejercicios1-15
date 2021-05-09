6c13cba382dd166007d25efe14f5781a614b36f2
Enrique <enriquesamudio1999@gmail.com>
2021-05-09 19:59:07 -0400

    Ejercicios 2, 3 y 4 listos

--- ejerciciosParteUno/Program.cs
+++ ejerciciosParteUno/Program.cs
@@ -7,14 +7,42 @@ namespace ejerciciosParteUno
         static void Main(string[] args)
         {
             // 1-) Mostrar los números impares entre el 0 y el 100
-            Console.WriteLine("Mostrar los números impares entre el 0 y el 100");
-            for (int i = 0; i <= 100; i++)
-            {
-                if (i % 2 != 0)
-                {
-                    Console.WriteLine("{0} es impar", i);
-                }
-            }
+            //Console.WriteLine("Mostrar los números impares entre el 0 y el 100");
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine("{0} es impar", i);
+            //    }
+            //}
+            // Fin Ejercicio 1
+
+            // 2-) Mostrar los números pares entre el 0 y el 100
+            //Console.WriteLine("Mostrar los números pares entre el 0 y el 100");
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine("{0} es par", i);
+            //    }
+            //}
+            // Fin Ejercicio 2
+
+            // 3-) Mostrar los números del 0 al 100
+            //Console.WriteLine("Mostrar los números del 0 al 100");
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    Console.WriteLine("Numero: {0}", i);
+            //}
+            // Fin Ejercicio 3
+
+            // 4-) Mostrar los números del 100 al 0
+            //Console.WriteLine("Mostrar los números del 100 al 0");
+            //for (int i = 100; i >= 0; i--)
+            //{
+            //    Console.WriteLine("Numero: {0}", i);
+            //}
+            // Fin Ejercicio 4
         }
     }
 }
