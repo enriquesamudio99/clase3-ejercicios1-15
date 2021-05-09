341c51447e8347459b532405096ae3963442eeeb
Enrique <enriquesamudio1999@gmail.com>
2021-05-09 19:34:39 -0400

    Ejercicio 1 Listo

--- ejerciciosParteUno/Program.cs
+++ ejerciciosParteUno/Program.cs
@@ -6,7 +6,15 @@ namespace ejerciciosParteUno
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1-) Mostrar los números impares entre el 0 y el 100
+            Console.WriteLine("Mostrar los números impares entre el 0 y el 100");
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("{0} es impar", i);
+                }
+            }
         }
     }
 }
