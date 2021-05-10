15f5d0d475262dab38d4b0286558bd95a9db4194
Enrique <enriquesamudio1999@gmail.com>
2021-05-09 22:34:21 -0400

    Más fallos arreglados

--- ejerciciosParteUno/Program.cs
+++ ejerciciosParteUno/Program.cs
@@ -18,6 +18,8 @@ namespace ejerciciosParteUno
                 }
             }
             // Fin Ejercicio 1
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 2-) Mostrar los números pares entre el 0 y el 100
             Console.WriteLine("=================================================================");
@@ -31,6 +33,8 @@ namespace ejerciciosParteUno
                 }
             }
             // Fin Ejercicio 2
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 3-) Mostrar los números del 0 al 100
             Console.WriteLine("=====================================================");
@@ -41,6 +45,8 @@ namespace ejerciciosParteUno
                 Console.WriteLine("Numero: {0}", i);
             }
             // Fin Ejercicio 3
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 4-) Mostrar los números del 100 al 0
             Console.WriteLine("=====================================================");
@@ -51,6 +57,8 @@ namespace ejerciciosParteUno
                 Console.WriteLine("Numero: {0}", i);
             }
             // Fin Ejercicio 4
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 5-) Mostrar los múltiplos de 3 y de 2 entre el 0 y 100
             Console.WriteLine("=======================================================================");
@@ -72,6 +80,8 @@ namespace ejerciciosParteUno
                 }
             }
             // Fin Ejercicio 5
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 6-) Ingresar un número y mostrar la suma de los números que lo anteceden. 
             Console.WriteLine("==========================================================================================");
@@ -86,6 +96,8 @@ namespace ejerciciosParteUno
             }
             Console.WriteLine("La suma de los números que lo anteceden es {0}", seisSumaAnteriores);
             // Fin Ejercicio 6
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 7-) Mostrar los números del 1 hasta el número ingresado.
             Console.WriteLine("=========================================================================");
@@ -98,6 +110,8 @@ namespace ejerciciosParteUno
                 Console.WriteLine("Número: {0}", i);
             }
             // Fin Ejercicio 7
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 8-) Contar los múltiplos de 3 desde la unidad hasta un número que ingresamos
             Console.WriteLine("==============================================================================================");
@@ -115,6 +129,8 @@ namespace ejerciciosParteUno
             }
             Console.WriteLine("Se conto {0} múltiplo/s de 3 hasta el número {1}", ochoMultiplosTres, ochoNumeroIngresado);
             // Fin Ejercicio 8
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 9-) Mostrar los números primos entre el 0 y 100.
             Console.WriteLine("=================================================================");
@@ -146,6 +162,8 @@ namespace ejerciciosParteUno
                 }
             }
             // Fin Ejercicio 9
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 10-) Ingresar dos números e intercambiarlos.
             Console.WriteLine("=============================================================");
@@ -162,6 +180,8 @@ namespace ejerciciosParteUno
             diezNumeroDos = diezAux;
             Console.WriteLine("El N° 1 es ahora el: {0}, y el N° 2 es ahora el: {1}", diezNumeroUno, diezNumeroDos);
             // Fin Ejercicio 10
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 11-) Ingresar un número y mostrar su cuadrado y cubo.
             Console.WriteLine("======================================================================");
@@ -172,6 +192,8 @@ namespace ejerciciosParteUno
             Console.WriteLine("El {0} elevado al cuadrado es {1}", onceNumeroIngresado, Math.Pow(onceNumeroIngresado, 2));
             Console.WriteLine("El {0} elevado al cubo es {1}", onceNumeroIngresado, Math.Pow(onceNumeroIngresado, 3));
             // Fin Ejercicio 11
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 12-) Ingresar dos números e indicar cuál es mayor y menor.
             Console.WriteLine("===========================================================================");
@@ -194,6 +216,8 @@ namespace ejerciciosParteUno
                 Console.WriteLine("El segundo número es mayor");
             }
             // Fin Ejercicio 12
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 13-) Ingresar dos números y mostrar la suma, la resta, la multiplicación, la división y el residuo entre ambos.
             // Para la división, se deberá determinar cuál es el mayor.
@@ -202,13 +226,13 @@ namespace ejerciciosParteUno
             Console.WriteLine("==  Para la división, se deberá determinar cuál es el mayor.                                                                  ==");
             Console.WriteLine("================================================================================================================================");
             Console.Write("Ingrese un primer numero: ");
-            int treceNumeroUno = Convert.ToInt32(Console.ReadLine());
+            double treceNumeroUno = Convert.ToDouble(Console.ReadLine());
             Console.Write("Ingrese un segundo numero: ");
-            int treceNumeroDos = Convert.ToInt32(Console.ReadLine());
+            double treceNumeroDos = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("La suma de ambos numeros es {0}", (treceNumeroUno + treceNumeroDos));
             Console.WriteLine("La resta de ambos numeros es {0}", (treceNumeroUno - treceNumeroDos));
             Console.WriteLine("La multiplicación de ambos numeros es {0}", (treceNumeroUno * treceNumeroDos));
-            decimal treceDivision = 0;
+            double treceDivision = 0;
             if (treceNumeroDos > treceNumeroUno)
             {
                 treceDivision = treceNumeroDos / treceNumeroUno;
@@ -221,6 +245,8 @@ namespace ejerciciosParteUno
             }
             Console.WriteLine("El residuo entre ambos numeros es {0}", (treceNumeroUno % treceNumeroDos));
             // Fin Ejercicio 13 
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 14-) Mostrar las tablas de multiplicar del 1 al 10. Se debe ver de esta manera: 1 x 1 = 1.
             Console.WriteLine("===========================================================================================================");
@@ -235,7 +261,9 @@ namespace ejerciciosParteUno
                     Console.WriteLine("{0} x {1} = {2}", i, j, (i * j));
                 }
             }
-            // Fin Ejercicio 14 
+            // Fin Ejercicio 14
+            Console.Write("Siguiente ejercicio");
+            Console.ReadLine();
 
             // 15-) Mostrar la tabla de multiplicar de un número ingresado por el usuario.
             Console.WriteLine("============================================================================================");
@@ -249,6 +277,7 @@ namespace ejerciciosParteUno
                 Console.WriteLine("{0} x {1} = {2}", quinceNumeroIngresado, i, (quinceNumeroIngresado * i));
             }
             // Fin Ejercicio 15 
+            Console.ReadLine();
         }
     }
 }
